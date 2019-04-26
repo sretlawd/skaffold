c9bb26b1980587b5385bc7795971eb5fe043e4ac
Priya Wadhwa <priyawadhwa@google.com>
2019-04-26 11:43:25 -0700

    move custom to testdata

--- integration/run_test.go
+++ integration/run_test.go
@@ -106,7 +106,7 @@ func TestRun(t *testing.T) {
 			remoteOnly:  true,
 		}, {
 			description: "custom builder",
-			dir:         "examples/custom",
+			dir:         "testdata/examples/custom",
 			pods:        []string{"bazel"},
 		},
 	}
